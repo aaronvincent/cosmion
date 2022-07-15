b261b9c5810e122d2005e61fa551c3c248c6c52c
aaronvincent <acvincent@gmail.com>
2022-07-15 10:04:36 -0400

    slurm

--- slurmrun.sh
+++ slurmrun.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-#SBATCH --array=0-20
+#SBATCH --array=101-500
 #SBATCH -c 1
 #SBATCH --job-name=transport
 #SBATCH --time=03:00:00
-#SBATCH --mem=1024
+#SBATCH --mem=512
 k=${SLURM_ARRAY_TASK_ID}
-fname=run1/positions${k}.dat
-./cosmion.x 5. 1.d-37 100000 ${fname}
+fname=runm800s33SI/positions${k}.dat
+./cosmion.x 0.8 1.d-33 100000 ${fname} 0
